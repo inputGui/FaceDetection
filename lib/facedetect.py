c11671322cf6523d4d336264cbe787138201a71e
GuiTx <86571679+inputGui@users.noreply.github.com>
2022-04-09 14:14:34 -0400

    Update for versatility and usability

--- lib/facedetect.py
+++ lib/facedetect.py
@@ -4,23 +4,23 @@ import mss.tools
 import cv2
 import time
 import mediapipe as mp
+from threading import Thread
 
 """
-Find faces in video.
+Personal assignment: Find faces in video.
 """
-
-confidence_input = None  # Placeholder for minimum confidence
 
 
 class FaceDetector:
-    def __init__(self, minDetectionConf=0.5):
+    def __init__(self, minDetectionConf=0.5, draw=True):
         self.minDetectionCon = minDetectionConf
         self.mpfacedetection = mp.solutions.face_detection
         self.mpdraw = mp.solutions.drawing_utils
         #  Initialize FaceDetection()
         self.facedetection = self.mpfacedetection.FaceDetection(self.minDetectionCon)
+        self.draw = True
 
-    def findFaces(self, img, draw=True):
+    def find_faces(self, img):
         img = np.flip(img[:, :, :3], 2)  # 1
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.facedetection.process(img)
@@ -34,7 +34,7 @@ class FaceDetector:
                 bbox = int(bboxC.xmin * image_w), int(bboxC.ymin * image_h), \
                        int(bboxC.width * image_w), int(bboxC.height * image_h)
                 bboxes.append([bbox, detection.score])
-                if draw:
+                if self.draw:
                     img = self.fancy_draw(img, bbox)
                     cv2.putText(img, f"{int(detection.score[0] * 100)}%", (bbox[0], bbox[1] - 20),
                                 cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 255), 3)
@@ -68,33 +68,60 @@ class FaceDetector:
     # Tell the other worker to stop
 
 
-def main():
-    """
-    Records and scans rect of screen
-    Displays to the user any detection of face
-    """
-    rect = (0, 40, 1000, 1300)
-    sct = mss.mss()
-    pTime = 0
-    detector = FaceDetector(confidence_input)
-    while True:
-        img = np.asarray(sct.grab(rect))
-        img, bboxes = detector.findFaces(img)
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-        cv2.putText(img, f"FPS: {int(fps)}", (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 3)
-        cv2.imshow('Video', img)
+class Video(Thread):
+    def __init__(self, confidence_input, draw=True, camera=False, dimensions=(0, 40, 1000, 1300)):
+        Thread.__init__(self)
+        self.start()
+        self.face_detector = FaceDetector(confidence_input, draw=draw)
+        self.dimensions = dimensions
+        self.camera = camera
+        self.draw = draw
+        self.monitor_cap = None
+        self.face_cap = None
 
-        if cv2.waitKey(15) & 0xFF == ord("q"):
-            cv2.destroyAllWindows()
-            break
+        if self.camera:
+            self.capture_cam(draw=self.draw)
+        else:
+            self.capture_monitor(draw=self.draw)
 
-    sct.close()
-    cv2.destroyAllWindows()
+    def capture_cam(self, draw: bool = True):
+        self.face_cap = cv2.VideoCapture(0)
+        pTime = 0
+        while True:
+            ret, frame = self.face_cap.read()
+            if ret:
+                img, bboxes = self.face_detector.find_faces(frame)
+                cTime = time.time()
+                fps = 1 / (cTime - pTime)
+                pTime = cTime
+                cv2.putText(img, f"FPS: {int(fps)}", (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 3)
+                cv2.imshow('Video', img)
+
+            if cv2.waitKey(15) & 0xFF == ord("q"):
+                cv2.destroyAllWindows()
+                break
+
+        self.face_cap.release()
+        cv2.destroyAllWindows()
+
+    def capture_monitor(self, draw: bool = True):
+        self.monitor_cap = mss.mss()
+        pTime = 0
+        while True:
+            img = np.asarray(self.monitor_cap.grab(self.dimensions))
+            img, bboxes = self.face_detector.find_faces(img)
+            cTime = time.time()
+            fps = 1 / (cTime - pTime)
+            pTime = cTime
+            cv2.putText(img, f"FPS: {int(fps)}", (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 3)
+            cv2.imshow('Video', img)
+
+            if cv2.waitKey(15) & 0xFF == ord("q"):
+                cv2.destroyAllWindows()
+                break
+
+        self.monitor_cap.close()
+        cv2.destroyAllWindows()
 
 
-if __name__ == "__main__":
-    confidence_input = float(input("Input desired minimum confidence level ( 0.1 - 0.9 ):"))
-    main()
-
+#image = Video(0.5, True, True)
